62733616b0ee3b2ef58f836ad80a6b821f706cd7
scq000 <scq000@hotmail.com>
2017-04-11 15:43:54 +0800

    add categories to filter

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { Product } from '../model/product.model';
 import { ProductRepository } from '../model/product.repository';
 
 @Component({
-  selector: 'store',
-  templateUrl: './store.component.html',
-  styleUrls: ['./store.component.css']
+    selector: 'store',
+    templateUrl: './store.component.html',
+    styleUrls: ['./store.component.css']
 })
 export class StoreComponent implements OnInit {
+    public selectedCategory = null;
 
-  constructor(private repository: ProductRepository) { }
+    constructor(private repository: ProductRepository) {}
 
-  ngOnInit() {}
+    ngOnInit() {}
 
-  get products(): Product[] {
-      return this.repository.getProducts();
-  }
+    get products(): Product[] {
+        return this.repository.getProducts(this.selectedCategory);
+    }
 
-  get categories(): string[] {
-      return this.repository.getCategories();
-  }
+    get categories(): string[] {
+        return this.repository.getCategories();
+    }
+
+    changeCategory(newCategory?: string) {
+        this.selectedCategory = newCategory;
+    }
 }
